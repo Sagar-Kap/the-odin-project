b6c4e6286aa5c89774b9c1a5f2ff20ba39494a1c
Sagar Kapoor <sagrkapr@gmail.com>
2022-01-04 13:15:53 +0200

    🚀 New Exercises

--- JS-Exercises/02_repeatString/repeatString.js
+++ JS-Exercises/02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    if(num<0){
+        return "ERROR";
+    }
+    else{return string.repeat(num);}
+    
 };
+
+repeatString('hey', 3);
+repeatString('hey', 10);
+repeatString('hey', 1);
+repeatString('hey', 0);
+repeatString("hey", -0);
 
 // Do not edit below this line
 module.exports = repeatString;
