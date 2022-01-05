1215a01740f840c052f48ba7f3bb5c23425a0b05
Sagar Kapoor <sagrkapr@gmail.com>
2022-01-05 09:02:53 +0200

    🚑️ JS Exercise, Remove from array Multiple, not working

--- JS-Exercises/04_removeFromArray/removeFromArray.js
+++ JS-Exercises/04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(array, ...object) {
+    let i = 0;
+    let j = 0;
+    while (i<array.length){
+        for (j;j<object.length;j++){
+            if (array[i]===object[j]){
+                array.splice(i,1);
+            }
+        }
+        i++;
+    } 
+    return array;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
