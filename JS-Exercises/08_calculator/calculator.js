052c0bbdd698ca3f95b4ca506618f8f3a9364cca
Sagar Kapoor <sagrkapr@gmail.com>
2022-01-06 18:08:27 +0200

    ✅ JS Exercise part 8

--- JS-Exercises/08_calculator/calculator.js
+++ JS-Exercises/08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum= 0;
+  for (let i=0 ; i<array.length; i++){
+    sum += array[i];    
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let multiple= 1;
+  for (let i=0 ; i<array.length; i++){
+    multiple = multiple*array[i];    
+  }
+  return multiple;      
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+    let power =1;
+    for (let i=1;i<=b;i++){
+      power =power*a;
+    }
+    return power;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let power =1;
+  for (let i=1;i<=a;i++){
+    power =power*i;
+  }
+  return power;  
 };
 
 // Do not edit below this line
