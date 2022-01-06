79a6ed878fcd01c42085db2e63ce47d99da352a4
Sagar Kapoor <sagrkapr@gmail.com>
2022-01-06 16:01:28 +0200

    ✅ Exercise 7 JS

--- JS-Exercises/07_tempConversion/tempConversion.js
+++ JS-Exercises/07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let tempCel = (temp-32)*(5/9);
+  return Math.round(tempCel*10)/10;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let tempFar = (temp*(9/5))+32;
+  return Math.round(tempFar*10)/10; 
 };
 
 // Do not edit below this line
